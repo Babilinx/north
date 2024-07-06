c1d4c0dc0a80648760af68447d818cc6c97f48e2
Babilinx <babilinx.evx1o@simplelogin.com>
2024-07-06 16:18:20 +0200

    feat(flake): add flake.nix
    Installs python3, nano and git

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = with pkgs; [
+         python3
+         nano
+         git
+        ];
+        
+      };
+
+    EDITOR = "nano";
+    
+    };
+}
